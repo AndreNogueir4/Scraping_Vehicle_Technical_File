04820d1efe41890c0423969f279223c1a59ef2b6
AndreNogueir4 <and.nogueira.dev@gmail.com>
2025-05-13 19:52:10 -0300

    Mudanças feitas no experiments, para rodar com playwright caso apareça o captcha

--- experiments/fichacompleta/scraper_automakers.py
+++ experiments/fichacompleta/scraper_automakers.py
@@ -1,4 +1,6 @@
 import aiohttp
+import asyncio
+from playwright.async_api import async_playwright
 import os
 from dotenv import load_dotenv
 from lxml import html
@@ -7,6 +9,25 @@ from unidecode import unidecode
 load_dotenv()
 
 words_to_remove = ['Quem Somos', 'Contato', 'Política de Privacidade', 'Ver mais']
+
+headers_mobile = {
+    'Host': 'www.fichacompleta.com.br',
+    'Sec-Ch-Ua': '"Chromium";v="127", "Not)A;Brand";v="99"',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Platform': '"Android"',
+    'Accept-Language': 'pt-BR',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko)'
+                  'Chrome/127.0.6533.100 Mobile Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;'
+              'q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Priority': 'u=0, i',
+    'Connection': 'keep-alive',
+}
 
 headers = {
     'Host': 'www.fichacompleta.com.br',
@@ -25,12 +46,45 @@ headers = {
     'Connection': 'keep-alive',
 }
 
+async def fetch_playwright(url: str, wait_time: int = 40) -> str:
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context(
+            user_agent='Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko)'
+                       'Chrome/127.0.6533.100 Mobile Safari/537.36'
+        )
+        page = await context.new_page()
+
+        await page.goto(url)
+        await asyncio.sleep(wait_time)
+
+        content = await page.content()
+        await browser.close()
+
+        return content
+
 async def get_automakers():
+    url = 'https://www.fichacompleta.com.br/carros/marcas/'
+
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://www.fichacompleta.com.br/carros/marcas/', headers=headers) as response:
+        async with session.get(url, headers=headers) as response:
 
             html_content = await response.text()
             tree = html.fromstring(html_content)
+
+            error_message = tree.xpath('//text()')
+
+            if 'Digite o código:' in error_message:
+                async with session.get(url, headers=headers_mobile) as response_mobile:
+                    html_content = await response_mobile.text()
+                    tree = html.fromstring(html_content)
+
+                    error_message_mobile = tree.xpath('//text()')
+
+                    if 'Digite o código:' in error_message_mobile:
+
+                        html_content = await fetch_playwright(url)
+                        tree = html.fromstring(html_content)
 
             automakers = tree.xpath('//div/a/text()')
             automakers = [unidecode(maker.lower().strip()) for maker in automakers if maker.strip() and maker.strip()
--- experiments/fichacompleta/scraper_version_and_years.py
+++ experiments/fichacompleta/scraper_version_and_years.py
@@ -1,12 +1,50 @@
 import aiohttp
 import re
 import os
+import asyncio
+from playwright.async_api import async_playwright
 from dotenv import load_dotenv
 from lxml import html
 
 load_dotenv()
 
 words_to_remove = ['Quem Somos', 'Contato', 'Política de Privacidade', 'Ver mais']
+
+headers_mobile = {
+    'Host': 'www.fichacompleta.com.br',
+    'Sec-Ch-Ua': '"Chromium";v="127", "Not)A;Brand";v="99"',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Platform': '"Android"',
+    'Accept-Language': 'pt-BR',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko)'
+                  'Chrome/127.0.6533.100 Mobile Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;'
+              'q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Priority': 'u=0, i',
+    'Connection': 'keep-alive',
+}
+
+async def fetch_playwright(url: str, wait_time: int = 40) -> str:
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context(
+            user_agent='Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko)'
+                       'Chrome/127.0.6533.100 Mobile Safari/537.36'
+        )
+        page = await context.new_page()
+
+        await page.goto(url)
+        await asyncio.sleep(wait_time)
+
+        content = await page.content()
+        await browser.close()
+
+        return content
 
 async def get_version_and_years(automaker, model):
     url = f'https://www.fichacompleta.com.br/carros/{automaker}/{model}/'
@@ -30,10 +68,24 @@ async def get_version_and_years(automaker, model):
     }
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(url, headers=headers) as response:
 
             html_content = await response.text()
             tree = html.fromstring(html_content)
+
+            error_message = tree.xpath('//text()')
+
+            if 'Digite o código:' in error_message:
+                async with session.get(url, headers=headers_mobile) as response_mobile:
+
+                    html_content = await response_mobile.text()
+                    tree = html.fromstring(html_content)
+
+                    error_message_mobile = tree.xpath('//text()')
+
+                    if 'Digite o código:' in error_message_mobile:
+                        html_content = await fetch_playwright(url)
+                        tree = html.fromstring(html_content)
 
             versions = {}
             years = []
@@ -79,7 +131,7 @@ async def get_version_and_years_proxy(automaker, model):
 
     async with aiohttp.ClientSession(connector=connector) as session:
         if proxy_url:
-            async with session.get(url, proxy=proxy_url) as response:
+            async with session.get(url, proxy=proxy_url, headers=headers) as response:
 
                 html_content = await response.text()
         else:
