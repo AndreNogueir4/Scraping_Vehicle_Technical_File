510518970aba5d4878bd5470c1da64d550109fe8
AndreNogueir4 <and.nogueria.dev@gmail.com>
2025-05-21 16:53:36 -0300

    Criacao do experiments/carrosweb, testar e validar

---
+++ experiments/carrosweb/scraper_models.py
@@ -0,0 +1,65 @@
+import requests
+import os
+from dotenv import load_dotenv
+from lxml import html
+from fake_useragent import UserAgent
+
+load_dotenv()
+PROXIES = os.getenv('PROXIES', '').split(',')
+
+automakers = 'chevrolet'
+words_to_remove = [
+    'Página Principal', 'Comparativo', 'Avaliação', 'Notícias', 'Opinião do Dono', 'Concessionárias',
+    'Ranking', 'Carros Mais Vendidos', 'Todos', 'Hatchback', 'Sedã', 'Perua', 'Minivan', 'Cupê',
+    'Conversível', 'SUV', 'Picape', 'Van', 'Furgão', 'Jipe', 'Chassi-cabine', 'Mapa do site',
+    'Sobre o site', 'Privacidade', 'Termos de uso', 'Mobile', 'Fale Conosco', 'Comunicar erro',
+    'Carros mais Vendidos', 'Próximos Lançamentos', '\r\n\t\t', 'Comparativos'
+]
+
+def generate_user_agent():
+    ua = UserAgent()
+    return ua.random
+
+def get_models(automaker, max_retries=5):
+    url = 'https://www.carrosnaweb.com.br/catalogofabricante.asp'
+    user_agent = generate_user_agent()
+
+    headers = {
+        'User-Agent': user_agent,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Referer': 'https://www.carrosnaweb.com.br/avancada.asp',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'DNT': '1',
+        'Sec-GPC': '1',
+        'Priority': 'u=0, i',
+    }
+
+    params = {
+        'fabricante': automaker
+    }
+
+    for proxy in PROXIES:
+        proxy_dict = {
+            'http': proxy,
+            'https': proxy
+        }
+        for attempt in range(1, max_retries + 1):
+            try:
+                response = requests.get(url, headers=headers, params=params, proxies=proxy_dict)
+
+                if response.status_code == 200:
+                    html_content = response.text
+                    print(html_content)
+                else:
+                    print(response.status_code)
+            except requests.RequestException as e:
+                print(f'Erro com proxy {proxy}: {e}')
+
+
+get_models(automakers)
